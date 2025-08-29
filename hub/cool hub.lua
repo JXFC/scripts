7aa177b771aaa042d5e34b3191e5ea250781d7c1
JXFC <xxdreamxxgamer@gmail.com>
2025-08-28 22:15:46 -0300

    Update cool hub.lua

--- hub/cool hub.lua
+++ hub/cool hub.lua
@@ -1,1 +1,83 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+Rayfield:Notify({
+   Title = "Hey!",
+   Content = "Works on some games",
+   Duration = 6.5,
+   Image = 4483362458,
+})
+local Window = Rayfield:CreateWindow({
+   Name = "cool hub",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "cooling down",
+   LoadingSubtitle = "by XDream205",
+   ShowText = "MVSD", -- for mobile users to unhide rayfield, change if you'd like
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
+   ToggleUIKeybind = "C", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+local Tab = Window:CreateTab("Main", 4483362458) -- Title, Image
+local Button = Tab:CreateButton({
+   Name = "Hitbox Manager GUI",
+   Callback = function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/HitboxExpander.lua"))()
+   -- The function that takes place when the button is pressed
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "ESP GUI (4layys)",
+   Callback = function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/4layy/4layy-s-Open-Source-ESP/refs/heads/main/SCRIPT"))()
+   -- The function that takes place when the button is pressed
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "FE Invisible GUI",
+   Callback = function()
+   loadstring(game:HttpGet('https://pastebin.com/raw/3Rnd9rHf'))()
+   -- The function that takes place when the button is pressed
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Delete UI",
+   Callback = function()
+   Rayfield:Destroy()
+   -- The function that takes place when the button is pressed
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Delete 4layys UI",
+   Callback = function()
+   local esp = game.CoreGui:FindFirstChild("ESP_UI_4layy")
+   if esp then
+     esp:Destroy()
+   end
+
+   -- The function that takes place when the button is pressed
+   end,
+})
